dfeb2c4b91b2b900a76a1620600a2bd455879fe3
Mykhailo Konstantinov <mkonst10@gmail.com>
2024-11-26 10:26:30 +0200

    remake LoadMore button to ts

--- src/components/LoadMoreBtn/LoadMoreBtn.tsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.tsx
@@ -1,6 +1,7 @@
 import styles from "./LoadMoreBtn.module.css";
+import { onLoadBtnProps } from "./LoadMoreBtn.types";
 
-const LoadMoreBtn = ({ onLoad }) => {
+const LoadMoreBtn = ({ onLoad }: onLoadBtnProps) => {
   return (
     <div className={styles.buttonWrapper}>
       <button className={styles.button} onClick={onLoad}>
---
+++ src/components/LoadMoreBtn/LoadMoreBtn.types.ts
@@ -0,0 +1,3 @@
+export type onLoadBtnProps = {
+  onLoad: () => void;
+};
