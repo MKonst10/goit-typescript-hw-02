e712a5c58c038ed9fba39a2e94fd402cee29bd83
Mykhailo Konstantinov <mkonst10@gmail.com>
2024-11-26 10:30:01 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import "./App.css";
 import SearchBar from "./components/SearchBar/SearchBar";
-import axios from "axios";
 import toast from "react-hot-toast";
 import ImageGallery from "./components/ImageGallery/ImageGallery";
 import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
